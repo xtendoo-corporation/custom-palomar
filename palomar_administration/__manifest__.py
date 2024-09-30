d23923cfc0665914d81053e74ca9d89266f4055e
Gokac9 <theabraham9@gmail.com>
2024-09-30 11:02:32 +0200

    first commit with module include

---
+++ palomar_administration/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Palomar Administration",
+    "summary": "Administración de Palomar",
+    "version": "17.0.1.0.0",
+    "description": "Administración de Palomar",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.es",
+    "depends": ['sale',
+                'base',
+                'sale_margin',
+                'product',
+                ],
+    "license": "AGPL-3",
+    "data": [
+        "security/security_group.xml",
+        "views/sale_order_view.xml",
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
