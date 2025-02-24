a5ee8948a0690ec2f368a34113c722711825d528
Gokac9 <theabraham9@gmail.com>
2025-02-24 09:43:09 +0100

    add module for invoice filter

---
+++ palomar_filter_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Palomar Filter Invoice",
+    "summary": "Palomar Filter Invoice",
+    "version": "17.0.1.0.0",
+    "description": "Palomar Filter Invoice",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.es",
+    "depends": [
+        'sale',
+        'base',
+    ],
+    "license": "AGPL-3",
+    "data": [
+        'views/account_move.xml',
+    ],
+    "installable": True,
+}
---
+++ palomar_filter_invoice/models/account_move.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    in_payment_line = fields.Boolean(
+        string='Remesada',
+        compute='_compute_in_payment_line',
+        store=True
+    )
+
+    @api.depends('line_ids')
+    def _compute_in_payment_line(self):
+        for move in self:
+            move.in_payment_line = any(
+                self.env['account.payment.line'].search([('move_id', '=', move.id)])
+            )
