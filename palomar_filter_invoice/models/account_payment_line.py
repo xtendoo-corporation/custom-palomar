157fabce8c9e95278821a47e37261d6c8f7ce33e
Gokac9 <theabraham9@gmail.com>
2025-02-25 12:01:17 +0100

    sync

--- palomar_filter_invoice/models/account_payment_line.py
+++ palomar_filter_invoice/models/account_payment_line.py
@@ -1,21 +1,21 @@
 from odoo import models, fields, api, _
 
-class AccountPaymentOrder(models.Model):
-    _inherit = 'account.payment.order'
+class AccountPaymentLine(models.Model):
+    _inherit = 'account.payment.line'
 
-    @api.onchange('payment_line_ids')
-    def _compute_payment_line(self):
-        for order in self:
-            print("*" * 50)
-            print('order', order)
-            print('payment_line_ids:', order.payment_line_ids)
-            for line in order.payment_line_ids:
+    @api.onchange('order_id')
+    def _onchange_order_id(self):
+        if self.order_id:
+            print("/" * 50)
+            print('order', self.order_id)
+            print('payment_line_ids:', self.order_id.payment_line_ids)
+            for line in self.order_id.payment_line_ids:
                 print('line:', line)
                 if line.move_line_id:
                     print('move_line_id:', line.move_line_id)
                     if line.move_line_id.move_id:
                         line.move_line_id.move_id.in_payment_line = True
-                        print("*" * 50)
+                        print("/" * 50)
                         print('line.move_line_id.move_id.in_payment_line', line.move_line_id.move_id.in_payment_line)
                     else:
                         print('move_line_id.move_id is None')
