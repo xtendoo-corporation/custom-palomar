b1310125349b0b982b0a49c7f0035e6452e173d0
Gokac9 <theabraham9@gmail.com>
2025-03-14 12:18:50 +0100

    add price unit in sale order line tree view

--- palomar_administration/__manifest__.py
+++ palomar_administration/__manifest__.py
@@ -19,6 +19,7 @@
         "views/product_view.xml",
         "views/account_move_report.xml",
         "views/account_move_views.xml",
+        "views/sale_order_line_tree_view.xml",
     ],
     "installable": True,
 }
