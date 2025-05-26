b9d0f6172222761f5c769885d17785ff9aafbc44
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-05-26 18:34:14 +0200

    precios de costo

--- palomar_validate_price/models/account_move.py
+++ palomar_validate_price/models/account_move.py
@@ -4,26 +4,18 @@ from odoo.exceptions import ValidationError
 class account_move(models.Model):
     _inherit = 'account.move'
 
-    def button_validate(self):
+    def action_post(self):
         for move in self:
             for line in move.invoice_line_ids:
                 if line.price_unit == 0:
                     raise ValidationError(
-                        'El precio del producto %s no puede ser 0.' % line.product_id.name
+                        f'El precio del producto {line.product_id.name} no puede ser 0.'
                     )
-
-                # Validar que el precio no sea negativo
-                print("*"*80)
+                print("*" * 80)
                 print("line.price_unit:", line.price_unit)
                 print("line.purchase_price:", line.purchase_price)
-
-                # Validar que el precio no sea menor que el coste
                 if line.price_unit < line.purchase_price:
                     raise ValidationError(
-                        'El precio del producto %s (%s) no puede ser inferior a su coste (%s).' % (
-                            line.product_id.name,
-                            round(line.price_unit, 2),
-                            round(line.line.purchase_price, 2)
-                        )
+                        f'El precio del producto {line.product_id.name} ({round(line.price_unit, 2)}) no puede ser inferior a su coste ({round(line.purchase_price, 2)}).'
                     )
-        return super(account_move, self).button_validate()
+        return super().action_post()
