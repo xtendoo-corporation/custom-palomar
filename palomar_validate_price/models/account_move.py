5b2686dc93c751d6370e0edf3dc2ba65adcabb42
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-05-27 08:20:19 +0200

    precio 0.00

--- palomar_validate_price/models/account_move.py
+++ palomar_validate_price/models/account_move.py
@@ -7,13 +7,10 @@ class account_move(models.Model):
     def action_post(self):
         for move in self:
             for line in move.invoice_line_ids:
-                if line.price_unit == 0:
+                if line.price_unit == 0.00:
                     raise ValidationError(
-                        f'El precio del producto {line.product_id.name} no puede ser 0.'
+                        f'El precio del producto {line.product_id.name} no puede ser 0.00. Por favor, revisa el producto y corrige su precio.'
                     )
-                print("*" * 80)
-                print("line.price_unit:", line.price_unit)
-                print("line.purchase_price:", line.purchase_price)
                 if line.price_unit < line.purchase_price:
                     raise ValidationError(
                         f'El precio del producto {line.product_id.name} ({round(line.price_unit, 2)}) no puede ser inferior a su coste ({round(line.purchase_price, 2)}).'
